74d5f911fa50893c67f1ce88e85d372d94902e64
hello <hello@ifnot.cc>
2018-12-15 16:03:15 +0800

    c: size

--- size.c
+++ size.c
@@ -2,5 +2,11 @@
 // Created by hello on 2018/12/14.
 //
 
+#include <stdio.h>
 
+int main(int argc, char **argv) {
+    printf("short int: %ld\n", sizeof(short int));
+    printf("int: %ld\n", sizeof(int));
+
+}
 
