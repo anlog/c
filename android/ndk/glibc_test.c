f5faca40b761f409051a238fcb3766395ff5c913
hello <hello@ifnot.cc>
2020-06-13 17:31:38 +0800

    add glibc_test(calloc malloc realloc)
    Signed-off-by: hello <hello@ifnot.cc>

--- android/ndk/glibc_test.c
+++ android/ndk/glibc_test.c
@@ -3,18 +3,36 @@
 //
 #include <stdio.h>
 #include <string.h>
-#include <memory.h>
+#include <stdlib.h>
 
 #define __USE_GNU
 
 void strchrs();
 
+void malloc_test();
+
 int main(void) {
     printf("glibc_test\n");
+
+    // test malloc realloc free
+    malloc_test();
 
 
     // test strchr strrchr strchrnul
     strchrs();
+}
+
+void malloc_test() {
+    void *pVoid = calloc(sizeof(int), 10);
+    printf("calloc: %lu - %p\n", sizeof(int), pVoid);
+    free(pVoid);
+    char *tmp = (char *) malloc(sizeof(char) * 10);
+    printf("malloc: %lu - %p size: %lu\n", sizeof(tmp), tmp, strlen(tmp));
+    *(tmp + 100) = 'a';
+    tmp = realloc(tmp, sizeof(char) * 10 * 10);
+    printf("remalloc: %lu - %p size: %lu\n", sizeof(tmp), tmp, strlen(tmp));
+
+    free(tmp);
 }
 
 const char *strchrnul(const char *str, char c) {
